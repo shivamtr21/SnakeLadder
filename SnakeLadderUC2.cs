bb209350385761a557d0ef0254df75777c9a5ed8
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-16 21:46:04 +0530

    Snake Ladder game

---
+++ SnakeLadderUC2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class SnakeLadderUC2
+    {
+        public static void DiceCall()
+        {
+            Console.WriteLine("Start the Snake and Ladder Game");
+            Console.WriteLine("Single player at start position 0");
+
+            Random random = new Random();
+            int Dice = random.Next(1, 6);
+            Console.WriteLine("Player 1 rolls the die and get : " + Dice);
+        }
+    }
+}
