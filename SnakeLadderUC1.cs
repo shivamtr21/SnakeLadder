33f5e8f95e1b97c8c5c570f8e6978b2b4fb003fc
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-16 21:42:20 +0530

    snake ladder game

---
+++ SnakeLadderUC1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class SnakeLadderUC1
+    {
+        public static void CallSnakeLadder()
+        {
+            Console.WriteLine("Start the Snake and Ladder Game");
+            Console.WriteLine("Single player at start position 0");
+        }
+    }
+}
