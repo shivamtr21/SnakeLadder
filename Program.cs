e6f1fc4516707002ab37239d05e063d6e0d975e0
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-16 21:39:21 +0530

    Snake Ladder game

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SnakeLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake ladder game");
+            //SnakeLadderUC1.CallSnakeLadder();
+            // SnakeLadderUC2.DiceCall();
+            //SnakeLadderUC3.CheckDiceChangePosition();
+            //SnakeLadderUC4.MaxPositionSnakeLadder();
+            //SnakeLadderUC6.MaxPositionSnakeLadder();
+           // SnakeLadderUC7.call();
+        }
+    }
+}
